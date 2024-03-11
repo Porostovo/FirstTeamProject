8475961c8cba99fd433cd76ff25182dee222cc65
Ondrej Jizba <ondrej.jizba@gmail.com>
2024-03-11 11:49:09 +0100

    b1

--- FirstTeamProject/src/Main.java
+++ FirstTeamProject/src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        count();
+    }
+
+    public static void count() {
+        System.out.println(29);
     }
 }
