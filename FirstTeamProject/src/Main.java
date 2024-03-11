3388b20eab2f3aeaf02aef8dbd4362e6dbe8f6ae
Sarka Ruzickova <sarka.ruzickova@gmail.com>
2024-03-11 11:49:49 +0100

    d1

--- FirstTeamProject/src/Main.java
+++ FirstTeamProject/src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+    future();
+    }
+
+    public static void future(){
+        System.out.println("Saggitarius");
     }
 }
