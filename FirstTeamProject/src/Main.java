57df70094a5e17ec98020f4c9b152600e61f300d
Gigerius <jachym.peterka@gmail.com>
2024-03-11 13:33:03 +0100

    a6_2

--- FirstTeamProject/src/Main.java
+++ FirstTeamProject/src/Main.java
@@ -4,28 +4,25 @@ public class Main {
     future("Saggitarius", 12);
     count();
     greet("hello", 5);
-    
     }
 
-<<<<<<< HEAD
-    public static void future(String zodiac, int month){
+    public static void future(String zodiac, int month) {
         System.out.println("Your zodiac is " + zodiac + "month" + month);
-
-=======
-    public static void future(){
-        System.out.println("Saggitarius");
->>>>>>> 63587bd16674bf87f5e8aeb17f066538904ca94d
     }
-    public static void cheer(){
+    public static void future () {
+        System.out.println("Saggitarius");
+    }
+    public static void cheer () {
         System.out.println("Hello Yellow Team!");
     }
 
-    public static void count() {
+    public static void count () {
         System.out.println(3);
     }
-    public static void greet(String hello, int howMany){
+    public static void greet (String hello,int howMany){
         for (int i = 0; i < howMany; i++) {
             System.out.println(hello);
         }
     }
+
 }
