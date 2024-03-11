fa228b40604e94ea48833f25520d5cbef4a99faf
Ondrej Jizba <ondrej.jizba@gmail.com>
2024-03-11 13:37:56 +0100

    b5_2

--- FirstTeamProject/src/Main.java
+++ FirstTeamProject/src/Main.java
@@ -1,41 +1,31 @@
 public class Main {
     public static void main(String[] args) {
-    cheer();
-<<<<<<< HEAD
-    future();
-    count(1994, 3);
-    greet();
-=======
-    future("Saggitarius", 12);
-    count();
-    greet("hello", 5);
->>>>>>> 57df70094a5e17ec98020f4c9b152600e61f300d
+        cheer();
+        count(1994, 3);
+        future("Saggitarius", 12);
+        greet("hello", 5);
     }
 
     public static void future(String zodiac, int month) {
         System.out.println("Your zodiac is " + zodiac + "month" + month);
     }
-    public static void future () {
+
+    public static void future() {
         System.out.println("Saggitarius");
     }
-    public static void cheer () {
+
+    public static void cheer() {
         System.out.println("Hello Yellow Team!");
     }
 
-<<<<<<< HEAD
     public static void count(int year, int month) {
         System.out.println(year + " " + month + " age: " + 29);
 
-
-=======
-    public static void count () {
-        System.out.println(3);
->>>>>>> 57df70094a5e17ec98020f4c9b152600e61f300d
     }
-    public static void greet (String hello,int howMany){
+
+    public static void greet(String hello, int howMany) {
         for (int i = 0; i < howMany; i++) {
             System.out.println(hello);
         }
     }
-
 }
